66d1712ed607ed4db391e32436d47ccfe422fa21
Natannegara_ <natannegara@gmail.com>
2022-08-11 09:15:36 +0800

    update workflow

--- git-diff.sh
+++ git-diff.sh
@@ -1,10 +1,12 @@
-#export diff=$(git diff --name-only HEAD^ HEAD)
-export diff=(/roles/object /inventory)
-export updated="projects/templates/"
-if [[ "$diff" == *"$updated"* ]]; then
-  echo $diff
-  echo true
-else
-  echo $diff
-  echo false
-fi
+export diff=$(git diff --name-only HEAD^ HEAD)
+export updated="projects/"
+for i in $diff; do
+  if [[ "$i" == "$updated"* ]]; then
+    echo true $i
+    echo "::set-output name=docs_changed::$i"
+    exit 0
+  else
+    echo false $i
+  fi
+done
+
