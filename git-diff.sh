7af382f0b629152e43814903a568198aa6158821
Natannegara_ <natannegara@gmail.com>
2022-08-11 08:39:51 +0800

    update

--- git-diff.sh
+++ git-diff.sh
@@ -1,1 +1,10 @@
-export diff=$(git diff --name-only HEAD^ HEAD)
+#export diff=$(git diff --name-only HEAD^ HEAD)
+export diff=(/roles/object /inventory)
+export updated="projects/templates/"
+if [[ "$diff" == *"$updated"* ]]; then
+  echo $diff
+  echo true
+else
+  echo $diff
+  echo false
+fi
